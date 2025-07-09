24d5a7ca1583386261b7cf23d635f68aa2b26fb7
renren <148751281+renrenbatabata@users.noreply.github.com>
2025-07-09 11:34:33 +0900

    Headerを追加

--- src/pages/StampListPage/StampListPage.tsx
+++ src/pages/StampListPage/StampListPage.tsx
@@ -1,11 +1,15 @@
 // import { useEffect, useState } from "react";
-// import styles from "./StampListPage.module.css";
+import styles from "./StampListPage.module.css";
 
 // 共通コンポーネント
+import header from "../../components/common/Header/Header.png";
 
 const StampListPage: React.FC = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <header>
+        <img src={header} alt="Header" className={styles.header} />
+      </header>
       <h1>スタンプ一覧ページ</h1>
     </div>
   );
