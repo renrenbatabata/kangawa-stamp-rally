e259762c8930f89d33b8160cca5a2b1cbef444ff
renren <148751281+renrenbatabata@users.noreply.github.com>
2025-07-09 11:24:48 +0900

    ホームページ（スタンプ状況画面）のファイル作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./styles/global.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import StampListPage from "./pages/StampListPage/StampListPage";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/stamps" element={<StampListPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/StampListPage/StampListPage.tsx
@@ -0,0 +1,14 @@
+// import { useEffect, useState } from "react";
+// import styles from "./StampListPage.module.css";
+
+// 共通コンポーネント
+
+const StampListPage: React.FC = () => {
+  return (
+    <div>
+      <h1>スタンプ一覧ページ</h1>
+    </div>
+  );
+};
+
+export default StampListPage;
