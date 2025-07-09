de5938cd8da6c891e629f4d0dfe21398169c28c3
renren <148751281+renrenbatabata@users.noreply.github.com>
2025-07-09 13:31:49 +0900

    スタンプ集めの進捗　作成

--- src/pages/StampListPage/StampListPage.tsx
+++ src/pages/StampListPage/StampListPage.tsx
@@ -4,14 +4,26 @@ import styles from "./StampListPage.module.css";
 // 共通コンポーネント
 import header from "../../components/common/Header/Header.png";
 
+// スタンプ画像
+import uncomplete from "../../assets/images/stamp_icon_uncomplete.png";
+import complete from "../../assets/images/stamp_icon_complete.png";
+
 const StampListPage: React.FC = () => {
   return (
     <div className={styles.container}>
       <header>
         <img src={header} alt="Header" className={styles.header} />
       </header>
-      <div>
+      <div className={styles.stampCompleteContainer}>
         <h1 className={styles.title}>スタンプ集めの進捗</h1>
+        <div className={styles.stampCompleteLists}>
+          <img src={uncomplete} alt="Uncomplete Stamp" />
+          <img src={uncomplete} alt="Uncomplete Stamp" />
+          <img src={uncomplete} alt="Uncomplete Stamp" />
+          <img src={uncomplete} alt="Uncomplete Stamp" />
+          <img src={uncomplete} alt="Uncomplete Stamp" />
+          <img src={complete} alt="Complete Stamp" />
+        </div>
       </div>
     </div>
   );
