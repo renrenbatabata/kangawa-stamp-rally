261e0af3babc57e625034a9cc9c73a8a722effb1
renren <148751281+renrenbatabata@users.noreply.github.com>
2025-07-10 13:01:33 +0900

    Nav遷移ルートを追加

--- src/components/common/FooterNav/FooterNav.tsx
+++ src/components/common/FooterNav/FooterNav.tsx
@@ -1,4 +1,5 @@
 import styles from "./FooterNav.module.css";
+import { useNavigate } from "react-router-dom";
 
 //アイコン
 import homeIcon from "./icon/home.png";
@@ -6,18 +7,35 @@ import mapIcon from "./icon/map.png";
 import cameraIcon from "./icon/camera.png";
 
 const FooterNav: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    navigate("/stamps");
+  };
+  const handleMapClick = () => {
+    navigate("/map");
+  };
+  const handleCameraClick = () => {
+    navigate("/camera");
+  };
   return (
     <div className={styles.footerNav}>
-      <button className={`${styles.footerNavItem} ${styles.home}`}>
+      <button
+        className={`${styles.footerNavItem} ${styles.home}`}
+        onClick={handleHomeClick}
+      >
         <img src={homeIcon} alt="Home" />
         <p>ホーム</p>
       </button>
-      <button className={styles.footerNavItem}>
+      <button className={styles.footerNavItem} onClick={handleCameraClick}>
         <div className={styles.camera}>
           <img src={cameraIcon} alt="Camera" />
         </div>
       </button>
-      <button className={`${styles.footerNavItem} ${styles.map}`}>
+      <button
+        className={`${styles.footerNavItem} ${styles.map}`}
+        onClick={handleMapClick}
+      >
         <img src={mapIcon} alt="Map" />
         <p>マップ</p>
       </button>
