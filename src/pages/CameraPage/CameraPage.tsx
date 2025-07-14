1857148025c1d978bfe2dadb6d3cbf5cd24f2275
renren <148751281+renrenbatabata@users.noreply.github.com>
2025-07-14 10:01:27 +0900

    FooternoCameraのCSS削除

--- src/pages/CameraPage/CameraPage.tsx
+++ src/pages/CameraPage/CameraPage.tsx
@@ -1,7 +1,12 @@
 import styles from "./CameraPage.module.css";
+import CameraButton from "../../components/common/CamaraButton/CameraButton";
 
 function CameraPage() {
-  return <div className={styles.cameraPage}>CameraPage</div>;
+  return (
+    <div className={styles.cameraPage}>
+      <CameraButton onClick={() => {}} />
+    </div>
+  );
 }
 
 export default CameraPage;
