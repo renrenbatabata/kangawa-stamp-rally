38b89539339b95296da4a075e8262482e949df8b
renren <148751281+renrenbatabata@users.noreply.github.com>
2025-07-22 13:55:40 +0900

    クイズ画面のUI作成

--- src/pages/Quizpage/QuizPage.tsx
+++ src/pages/Quizpage/QuizPage.tsx
@@ -1,10 +1,32 @@
-// import styles from './QuizPage.module.css';
+import styles from "./QuizPage.module.css";
+import background from "../../assets/images/background.png";
 
 const QuizPage: React.FC = () => {
+  const quizData = {
+    id: 1,
+    question: "かながわ区の『区の木』はどんな木でしょうか？",
+    options: ["さくら", "いちょう", "もみじ", "くすのき"],
+    answer: "さくら", // 正解の選択肢
+  };
+
   return (
-    <div>
-      <h1>Quiz Page</h1>
-      {/* QuizPageのコンテンツをここに追加 */}
+    <div
+      className={styles.quizPage}
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className={styles.quizContent}>
+        <h1 className={styles.quizTitle}>💡かながわくクイズ</h1>
+        <h2 className={styles.quizQuestion}>{quizData.question}</h2>
+        <ul className={styles.quizOptions}>
+          {quizData.options.map((option, index) => (
+            <li key={index} className={styles.quizOption}>
+              {option}
+            </li>
+          ))}
+        </ul>
+
+        <button className={styles.quizButton}>こたえをみる！</button>
+      </div>
     </div>
   );
 };
