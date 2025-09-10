c83d4460d89b13f739e379bb64608b1b0b452c3d
renrenbatabata <renrenbatabata4869@gmail.com>
2025-09-11 05:34:42 +0900

    追加進捗バー

--- src/components/common/StampBadge/StampBadge.tsx
+++ src/components/common/StampBadge/StampBadge.tsx
@@ -1,17 +1,41 @@
 import styles from "./StampBadge.module.css";
+import { GiTurtle } from "react-icons/gi";
 
 type StampBadgeProps = {
   progress: number;
 };
 
 const StampBadge = ({ progress }: StampBadgeProps) => {
+  // progress の数だけ、カメのアイコンを生成します。
+  const progressIcons = Array.from({ length: 4 }, (_, index) => (
+    <div key={index} className={styles.iconWrapper}>
+      <GiTurtle className={index < progress ? styles.checkedIcon : styles.unCheckedIcon} />
+    </div>
+  ));
+
+  const progressBarWidth = (progress / 4) * 100;
+
   return (
     <div className={styles.Container}>
-      <p className={styles.progressKazu}>取得したスタンプ数</p>
+      <p className={styles.progressKazu}>スタンプ進捗</p>
+      
       <div className={styles.progressConteinae}>
         <span className={styles.progress}>{progress}</span>
         <span className={styles.progressSeparator}>/</span>
         <span className={styles.progressTotal}>4</span>
+      </div>
+      
+      {/* プログレスバーを配置 */}
+      <div className={styles.progressBarContainer}>
+        <div 
+          className={styles.progressBarFill} 
+          style={{ width: `${progressBarWidth}%` }}
+        ></div>
+      </div>
+
+      {/* アイコンを進捗バーの下に配置します */}
+      <div className={styles.iconsContainer}>
+        {progressIcons}
       </div>
     </div>
   );
