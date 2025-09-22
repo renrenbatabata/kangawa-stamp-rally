0bc921c9ae498b026ed4978426ebbb2eea97ba99
renren <148751281+renrenbatabata@users.noreply.github.com>
2025-09-22 11:22:48 +0900

    修正　UUID

--- src/UserContext.tsx
+++ src/UserContext.tsx
@@ -1,18 +1,9 @@
 // src/context/UserContext.ts
-import { createContext, useContext } from "react"; // ReactのContext関連フックをインポート
-
-// UIDをlocalStorageに保存するためのキーをエクスポート
-export const UID_LOCAL_STORAGE_KEY = "kanagawa_qr_uid"; // 他のアプリと衝突しないユニークなキー名を推奨
-
-// UserContextの定義とエクスポート
-// コンテキストの初期値はnull。Providerがない場合はnullが返ることを示す。
+import { createContext, useContext } from "react";
+export const UID_LOCAL_STORAGE_KEY = "kanagawa_qr_uid";
 export const UserContext = createContext<string | null>(null);
-
-// UserContextを利用するためのカスタムフック
-// このフックもエクスポートします
 export const useUserContext = () => {
   const context = useContext(UserContext);
-  // ContextがUserProviderで提供されていない場所でフックが使われた場合にエラーをスロー
   if (context === null) {
     throw new Error(
       "useUserContext は UserProvider の中で使用する必要があります"
