b8c0db1e236518472179c39102e105c5f22387bc
renrenbatabata <renrenbatabata4869@gmail.com>
2025-09-23 18:21:25 +0900

    追加　スタンプ説明

--- src/components/StampCard/StampCard.tsx
+++ src/components/StampCard/StampCard.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import styles from "./StampCard.module.css";
 
 interface StampCardProps {
   id: string;
   title: string;
   imgPath: string;
+  stampText: string;
 }
 
-// Stamp card component
-const StampCard: React.FC<StampCardProps> = ({ title, imgPath }) => {
-  return (
-    <div className={styles.stampCard}>
+const StampCard: React.FC<StampCardProps> = ({ title, imgPath, stampText }) => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const handleCardClick = () => {
+    setIsPopupVisible(true);
+  };
+
+  const handleClosePopup = () => {
+    setIsPopupVisible(false);
+  };
+
+return (
+  <>
+    <button type = "button" className={styles.stampCard} onClick={handleCardClick}>
       <div className={styles.stampInfo}>
         <p className={styles.stampStatus}>
             <img
@@ -20,8 +32,22 @@ const StampCard: React.FC<StampCardProps> = ({ title, imgPath }) => {
         </p>
         <h2 className={styles.stampTitle}>{title}</h2>
       </div>
-    </div>
-  );
+    </button>
+
+    {isPopupVisible && (
+      <div className={styles.popupOverlay}>
+        <div className={styles.popup}>
+          <button type = "button" className={styles.closeButton} onClick={handleClosePopup}>
+            &times;
+          </button>
+          <img src={imgPath} alt="Stamp Logo" className={styles.popupLogo} />
+          <div className={styles.popupTitle}>{title}</div>
+          <p className={styles.popupText}>{stampText}</p>
+        </div>
+      </div>
+    )}
+  </>
+);
 };
 
 export default StampCard;
--- src/components/StampList/StampList.tsx
+++ src/components/StampList/StampList.tsx
@@ -10,10 +10,10 @@ type StampListProps = {
 
 const StampList: React.FC<StampListProps> = ({ stamps }) => {
   const fixedStamps = [
-    { stampNo: "stamp_point_1", stampName: "スタンプ1" },
-    { stampNo: "stamp_point_2", stampName: "スタンプ2" },
-    { stampNo: "stamp_point_3", stampName: "スタンプ3" },
-    {stampNo: "stamp_point_4", stampName: "スタンプ4" },
+    { stampNo: "stamp_point_1", stampName: "スタンプ1", stampText: "反町公園：色とりどりのチューリップが咲き誇り、うららかな春の訪れを教えてくれる。" },
+    { stampNo: "stamp_point_2", stampName: "スタンプ2", stampText: "片倉うさぎ山公園：春の兆しを感じるころ、走り回る子どもたちを満開のこぶしが見守ります。" },
+    { stampNo: "stamp_point_3", stampName: "スタンプ3", stampText: "三ツ沢せせらぎ緑道：静けさに響くせせらぎとアジサイが奏でるハーモニー。" },
+    { stampNo: "stamp_point_4", stampName: "スタンプ4", stampText: "三ツ沢公園：バラ園には季節ごとに様々な種類のバラが咲き、華やかな香り、色を楽しめます。" },
   ];
 
   return (
@@ -29,6 +29,7 @@ const StampList: React.FC<StampListProps> = ({ stamps }) => {
                 id={foundStamp.stampNo}
                 title={foundStamp.stampName}
                 imgPath={foundStamp.imgPath}
+                stampText={foundStamp.stampText}
               />
             );
           } else {
@@ -39,6 +40,7 @@ const StampList: React.FC<StampListProps> = ({ stamps }) => {
                 id={fixedStamp.stampNo}
                 title={fixedStamp.stampName}
                 imgPath={displayImgPath}
+                stampText={fixedStamp.stampText}
               />
             );
           }
