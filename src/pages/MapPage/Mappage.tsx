d090a44b45ccbae43e5a7f7089d069dee8e8dbe6
renrenbatabata <renrenbatabata4869@gmail.com>
2025-09-23 18:20:59 +0900

    修正　フッター　Path

--- src/pages/MapPage/Mappage.tsx
+++ src/pages/MapPage/Mappage.tsx
@@ -5,14 +5,11 @@ import FooterNav from "../../components/common/FooterNav/FooterNav";
 const MapPage: React.FC = () => {
   return (
     <div>
-      {/* ヘッダーコンポーネント */}
       <Header />
 
-      {/* マップのコンテンツ */}
       <p>マップコンテンツ入れる</p>
 
-      {/* フッターのナビゲーション */}
-      <FooterNav />
+      <FooterNav homePath="/stamps" cameraPath="/scan" mapPath="/map" />
     </div>
   );
 };
